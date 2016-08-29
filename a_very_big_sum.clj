9085cde66d0153e1768b878df1c96d150e2b0503
Marco Dalla Stella <marco@7bridges.eu>
2016-08-30 00:35:33 +0200

    Fixed typo on A Very Big Sum.

--- a_very_big_sum.clj
+++ a_very_big_sum.clj
@@ -4,7 +4,7 @@
 
 (let [n_t (read-line)
       n (Integer/parseInt n_t)]
-  (let [arr_temp (rsead-line)
+  (let [arr_temp (read-line)
         arr_t (split arr_temp #"\s+")
         arr (map #(Integer/parseInt %) arr_t)]
     (prn (apply + arr))))
